cd0193f07466a74e7b0949f0f8e202e37e7875f1
Sakshi Jain <sakshi.172015@gmail.com>
2022-07-14 11:19:49 +0530

    api call for getAllQuizzesTitles

--- Front-End/React-Online-Quiz-Application/src/components/ShowAllQuizzes.js
+++ Front-End/React-Online-Quiz-Application/src/components/ShowAllQuizzes.js
@@ -1,33 +1,40 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Grid } from "@mui/material";
 import Button from "@mui/material/Button";
+import {getAllQuizzesTitles} from '../services/quiz.service'
 
 
 export default function AllQuizzes() {
-  const arr = [1,2,3,4]
-  return (
-	<div className='p-5'>
-    <Grid container spacing={2} style={{display: "flex", flexDirection: "row"}}>
-      {
-        arr.map(()=>{
-          return(
-            <Grid item xs={6} md={4} lg={3}>
-				<div className='d-flex align-items-center p-2 flex-column' style={{backgroundColor: "#e3e3e3"}}>
-					<h4>Quiz Name</h4>
-					<span>Time Limit: 20 minutes</span>
-					<Button
-						className="my-3 px-5"
-						variant="contained"
-						style={{backgroundColor: "#533b7c"}}
-					>
-						Start
-					</Button>
-				</div>
-			</Grid>
-          )
-        })
-      }
-    </Grid>
-	</div>
-  )
+    const [data, setData] = useState([])
+    useEffect(async() => {
+      const res = await getAllQuizzesTitles()
+      console.log(res.data)
+      setData(res.data)
+    }, [])
+    
+    return (
+        <div className='p-5'>
+        <Grid container spacing={2} style={{display: "flex", flexDirection: "row"}}>
+        {
+            data.map((item)=>{
+            return(
+                <Grid item xs={6} md={4} lg={3}>
+                    <div className='d-flex align-items-center p-2 flex-column' style={{backgroundColor: "#e3e3e3"}}>
+                        <h4>{item.title}</h4>
+                        <span>Time Limit: 20 minutes</span>
+                        <Button
+                            className="my-3 px-5"
+                            variant="contained"
+                            style={{backgroundColor: "#533b7c"}}
+                        >
+                            Start
+                        </Button>
+                    </div>
+                </Grid>
+            )
+            })
+        }
+        </Grid>
+        </div>
+    )
 }
--- Front-End/React-Online-Quiz-Application/src/services/quiz.service.js
+++ Front-End/React-Online-Quiz-Application/src/services/quiz.service.js
@@ -4,21 +4,25 @@ import authHeader from "./auth-header";
 const API_URL = "http://localhost:8299/quiz-service/api/";
 
 export const postQuiz = async (data) => {
-  console.log(data);
-  try {
-    return axios.post(API_URL + "addQuiz", data, {
-      headers: authHeader(),
-      "Content-Type": "application/json",
-    });
-  } catch (err) {
-    console.log(err);
-    return [];
-  }
+	console.log(data);
+	try {
+		return axios.post(API_URL + "addQuiz", data, {
+		headers: authHeader(),
+		"Content-Type": "application/json",
+		});
+	} catch (err) {
+		console.log(err);
+		return [];
+	}
 };
-export const getAllQuizzes = async () => {
-  return axios.get(
-    API_URL + "getAllQuizzes",
-
-    { headers: authHeader() }
-  );
+export const getAllQuizzes = async () => { 
+	return axios.get(
+		API_URL + "getAllQuizzes",
+		{ headers: authHeader() }
+	);
 };
+export const getAllQuizzesTitles = async() => {
+	return axios.get(API_URL + "getAllTitles",
+		{ headers: authHeader() }
+	)
+}
